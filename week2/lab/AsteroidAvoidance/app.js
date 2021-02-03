eef9fc2b0d713b77b3927baac972ab0f0d6cdce9
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 10:28:02 -0500

    lab1 modifications

--- week2/lab/AsteroidAvoidance/app.js
+++ week2/lab/AsteroidAvoidance/app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var mongoose = require('mongoose')
 var path = require('path')
 var port = 5000
 
@@ -15,3 +16,24 @@ app.use(express.static(__dirname + '/views'))
 app.listen(port,function(){
     console.log("Listening on port 5000")
 })
+
+mongoose.connect('mongodb://localhost:27017/gameEntries',{
+    useNewUrlParser:true
+}).then(function()
+{
+    console.log("Connected to Mongo data")
+}).catch(function(err){
+    console.log(err)
+})
+
+app.post('/saveGame',function(req,res){
+    console.log("request has been made")
+    console.log(req.body)
+    
+    })
+    
+    app.get('/getData',function(req,res){
+        Game.find({}).then(function(game){
+            res.json({game})
+        })
+    })
--- week3/app.js
+++ week3/app.js
@@ -15,22 +15,95 @@ serv.listen(3000, function () {
     console.log('Connected on localhost 3000')
 })
 
+var SocketList = {}
+var PlayerList = {}
 
+var Player = function(id)
+{
+    var self = {
+        x:400,
+        y:300,
+        id:id,
+        number: Math.floor(Math.random()*10),
+        right: false,
+        left: false,
+        up: false,
+        down: false,
+        speed:10
+    }
+    self.updatePosition =function()
+    {
+        if(self.right)
+        self.x += self.speed
+        if(self.left)
+        self.x -= self.speed
+        if(self.up)
+        self.y -= self.speed
+        if(self.down)
+        self.y += self.speed
+    }
+    return self
+}
 //server side comm
 io.sockets.on('connection', function (socket) {
     console.log("socket connected")
 
-    socket.on('sendMsg', function (data) {
-        console.log(data.message)
+    socket.id = Math.random()
+    // socket.x = 0
+    // socket.y = Math.floor(Math.random()*600)
+    // socket.number = Math.floor(Math.random()*10)
+    //add something to socketlist
+    SocketList[socket.id]= socket 
+
+    var player = new Player(socket.id)
+    PlayerList[socket.id]= player
+
+    socket.on('disconnect', function(){
+        delete SocketList[socket.id]
+        delete PlayerList[player.id]
     })
 
-    socket.on('sendBtnMsg',function(data){
-        console.log(data.message)
+    socket.on('keypress', function(data){
+        if(data.inputId === "up")
+        player.up=data.state
+        if(data.inputId  === "down")
+        player.down=data.state
+        if(data.inputId  === "left")
+        player.left=data.state
+        if(data.inputId  === "right")
+        player.right=data.state
     })
 
-    socket.emit('messageFromServer', {
-        message: 'AA EE OO'
-    })
+    //old example from wednesday
+    //socket.on('sendMsg', function (data) {
+        //console.log(data.message)
+  //  })
+
+    //socket.on('sendBtnMsg',function(data){
+       // console.log(data.message)
+  //  })
+
+   // socket.emit('messageFromServer', {
+        //message: 'AA EE OO'
+  //  })
 
 
 })
+
+//setup update loop
+setInterval(function(){
+ var pack = []
+    for(var i in PlayerList){
+    var player = PlayerList[i]
+    player.updatePosition()
+    pack.push({
+        x:player.x,
+        y:player.y,
+        number:player.number
+    })
+}
+for(var i in SocketList){
+    var socket = SocketList[i]
+    socket.emit('newPosition', pack)
+}
+},1000/30)
